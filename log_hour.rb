cf937498c9247507b9489be29cc32c2c2f440c12
Jingyiping Zhang <jzhang12@cs.brown.edu>
2016-10-14 20:07:10 -0400

    GOOGLE OFFICIAL SAMPLE DOC GUIDANCE

--- log_hour.rb
+++ log_hour.rb
@@ -1,12 +1,27 @@
 #!/usr/bin/env ruby
 
+
+"- Extract from Google Online Spreadsheet
+ - Prepare the message (Date Range, Logged Hours, Special Notes, Body Template)
+ - Send the email"
 require 'dotenv'
 require 'gmail'
+require 'net/https'
+
 
 Dotenv.load
 
 GMAIL_USERNAME = ENV['GMAIL_USERNAME']
 GMAIL_PASSWORD = ENV['GMAIL_PASSWORD']
+
+http = NET::HTTP.new('www.google.com',443)
+http.user_ssl = true
+path = '/account/ClientLogin'
+data = 'accountType=HOSTED_OR_GOOGLE'.'&Email='.GMAIL_USERNAME.'&Passwd='GMAIL_PASSWORD.'&service=wise'
+
+
+
+
 
 GMAIL = Gmail.connect(GMAIL_USERNAME, GMAIL_PASSWORD)
 #KUMARS_EMAIL = 'kumar.a@example.com'
@@ -16,12 +31,13 @@ RECIPIENT_EMAIL = 'jingyiping_zhang@brown.edu;jzhang12@cs.brown.edu'
 DB_NAME_REGEX  = /\S+_staging/
 KEYWORDS_REGEX = /sorry|help|wrong/i
 
-def create_reply(subject)
+def create_reply(dateRange,specialNotes,hours)
   GMAIL.compose do
     to RECIPIENT_EMAIL
-    subject "[EMCS2000] Log Hours #{subject}"
-    body "Hi,\n The followings are my hours for week #{subject}:
-"
+    subject "[EMCS2000] Log Hours #{dateRange}"
+    body "Hi,\n The followings are my hours for week #{dateRange}:\n
+    Note:#{specialNotes}\n
+    Regards,\n Jingyiping"
   end
 end
 
@@ -40,3 +56,7 @@ GMAIL.inbox.find(:unread, from: KUMARS_EMAIL).each do |email|
     GMAIL.deliver(reply)
   end
 end
+
+
+	reply = create_reply(email.subject)
+    GMAIL.deliver(reply)
